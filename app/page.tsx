71fad9cbe8cfe89e9f61877295bcb08adfd5ad4b
Darren Chan <darrenchan@Darrens-MacBook-Air.local>
2025-09-29 16:17:55 +0800

    v

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import Hero from "@/components/Hero";
-import Section from "@/components/Section";
-import PersonCard from "@/components/PersonCard";
-import PracticeCard from "@/components/PracticeCard";
-import { people } from "@/data/people";
-import { practiceAreas } from "@/data/practice-areas";
 import { getSortedPosts } from "@/lib/posts";
-import PostCard from "@/components/PostCard";
 import HeroImage from "@/components/HeroImage";
 import Testimonials from "@/components/Testimonials";
 import WhyChooseUs from "@/components/WhyChooseUs";
